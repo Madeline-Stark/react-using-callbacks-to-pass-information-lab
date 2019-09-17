de045d7e2714a2dc03cce7b15aa1ba3464127ee1
Madeline-Stark <madeline.stark1@gmail.com>
2019-09-16 17:21:21 -0700

    Done.

--- src/ColorSelector.js
+++ src/ColorSelector.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 
 export default class ColorSelector extends Component {
-  
+
+  //test that lab works by clicking color then clicking cell on chrome boi
+
   makeColorSwatches = () => (
     ["#F00", "#F80", "#FF0", "#0F0", "#00F", "#508", "#90D", "#FFF", "#000"].map((str, idx) => {
-      return <div key={idx} className="color-swatch" style={{backgroundColor: str}}/>
+      return <div key={idx} onClick={() => {this.props.changeColor(str)}} className="color-swatch" style={{backgroundColor: str}}/>
     })
+    //issue b/c was calling changecolor every time so getting constantly updating state error
+    //when passing any function with an argument to on click, needs to be inside anonymous arrow function
+    //otherwise () will cause constant invocation
   )
-  
+  //makeColorSwatches is what creates the divs for each color,
+  //want event handler in there
+  //this.props.changeColor(str) pass in str of color to the callback function
+
+
   render() {
     return (
       <div id="colorSelector">
@@ -15,5 +24,5 @@ export default class ColorSelector extends Component {
       </div>
     )
   }
-  
+
 }
--- src/Matrix.js
+++ src/Matrix.js
@@ -6,11 +6,28 @@ import ColorSelector from './ColorSelector.js'
 export default class Matrix extends Component {
 
   constructor() {
-    super()
+    super()//would pass in props if in parent component were passing in props
+    this.state = {
+      selectedColor: '#fff'
+    }
   }
 
+  changeColor = (color) => {
+    this.setState({selectedColor: color})
+  }
+
+//   In React, props are used to pass information down the component tree, from parents to children. In order to propagate information in the opposite direction, we can use callback functions that are passed from parent components to children.
+//
+// This allows the callback to be owned by a different component than the one invoking it. Once invoked, the callback can effect change in the component that owns it, instead of the component that called it.
+//use color selector to select color
+//pass in function as prop
+
+getColor = () => { //don't want you passing in color as prop, want callback function instead
+  return this.state.selectedColor
+}
+
   genRow = (vals) => (
-    vals.map((val, idx) => <Cell key={idx} color={val} />)
+    vals.map((val, idx) => <Cell key={idx} color={val} getColor={this.getColor} />) //no (), dont want to invoke yet
   )
 
   genMatrix = () => (
@@ -21,7 +38,7 @@ export default class Matrix extends Component {
   render() {
     return (
       <div id="app">
-        <ColorSelector />
+        <ColorSelector changeColor={this.changeColor}/>
         <div id="matrix">
           {this.genMatrix()}
         </div>
